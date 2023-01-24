12429f23f60901d844d9abe575dca062f9c5609d
admin <admin@admins-MacBook-Pro.local>
2023-01-24 17:45:32 +0000

    local storage is working

--- js/script.js
+++ js/script.js
@@ -1,47 +1,58 @@
+
+var localStorageInput = JSON.parse(localStorage.getItem('userInput'));
 //showing the current date using moment api and adding it to the currentday element
 const now = moment().format('MMMM Do YYYY');
 const timeNow = $('#currentDay');
-timeNow.text(now.toString());
+timeNow.text(now.toString()); //it displays the current day
 var saveBtn = $('button');
 
-var userInput = [];
+var userInput = localStorageInput || []; //storing user input and timeblock here
 
 // this function is adding colour codes to the timeblock, based on the current time
 var blocks = $('input');
 
-
 blocks.each(function() {
   
     var thisHour = moment().format('k');
-    console.log(thisHour);
-
-    var hour = $(this).attr('id');
-
-    if (hour < thisHour) {
+    
+    var hour = $(this).attr('data-hour');
+    
+    if (thisHour > hour) {
         $(this).css('background-color', '#d3d3d3');
     }
-    else if (hour === thisHour) {
+    else if (thisHour === hour) {
         $(this).css('background-color', '#ff6961');
     }
-    else if (hour > thisHour) {
+    else if (thisHour < hour) {
         $(this).css('background-color', '#77dd77');
     }
 
 }); 
+    
+        saveBtn.click(function (event) {
+            event.preventDefault();
+            var inputText = $(this).parent().siblings('input').val();
+            console.log(inputText);
+            userInput.push({input: inputText, time: $(this).attr('id') });
+            console.log(userInput);
+            localStorage.setItem( "userInput", JSON.stringify(userInput));
+            $('#message').toggle(1000);
+          });
 
 
-saveBtn.click(function (event) {
-    event.preventDefault();
-    var inputText = $('input:text').val();
-    console.log(inputText);
-    userInput.push({input: inputText, time: $(this).attr('id') });
-    console.log(userInput);
-    localStorage.setItem( "value", JSON.stringify(userInput));
-   
-  });
+    localStorageInput.each(function(input){
+       
+    });
 
 
-// 4. Load input from local storage when page load/refresh if there's any data in local storage
+  
+
+ 
+ 
+
+
+    
+    // 4. Load input from local storage when page load/refresh if there's any data in local storage
 //      var localStorageInput = get data from local storage
 //      check if localStorageInput exist, if it is
 //          var parsedLocalStorageInput = parse localStorageInput
