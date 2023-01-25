e86a8f579e4d46f3ac5b5fdc587998a8d2f090e5
admin <admin@admins-MacBook-Pro.local>
2023-01-25 12:24:31 +0000

    added comments

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-
+//getting data from local storage
 var localStorageInput = JSON.parse(localStorage.getItem('userInput'));
 //showing the current date using moment api and adding it to the currentday element
 const now = moment().format('MMMM Do YYYY');
@@ -28,42 +28,17 @@ blocks.each(function() {
     }
 
 }); 
-    
+    //this click event pushes the user input and the timeblock to the userinput array
+    //and stores them in the local storage
         saveBtn.click(function (event) {
             event.preventDefault();
             var inputText = $(this).parent().siblings('input').val();
             console.log(inputText);
-            userInput.push({input: inputText, time: $(this).attr('id') });
+            var timeblockValue = $(this).attr('id');
+            userInput.push({input: inputText, time: timeblockValue });
             console.log(userInput);
             localStorage.setItem( "userInput", JSON.stringify(userInput));
-            $('#message').toggle(1000);
+            
           });
 
 
-    localStorageInput.each(function(input){
-       
-    });
-
-
-  
-
- 
- 
-
-
-    
-    // 4. Load input from local storage when page load/refresh if there's any data in local storage
-//      var localStorageInput = get data from local storage
-//      check if localStorageInput exist, if it is
-//          var parsedLocalStorageInput = parse localStorageInput
-//          populate the inputs with the value from parsedLocalStorageInput
-//          inputs.forEach(function(input) {
-//              input => {
-//                time: 9,
-//                input: 'Meeting',
-//              }
-//              $('.timeblock[data-hour="' + input.time + '"] textarea').val(input.input)
-//          })
-//      if theres no data in local storage, do nothing
-
-// Extra, add hover effect on the save button
